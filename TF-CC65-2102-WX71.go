4e3ac4559ddfde265f5256672a02da34e7b53df0
ivan3L <ivanluiguilopezlopez@gmail.com>
2021-11-19 00:15:45 -0500

    added data processing

--- TF-CC65-2102-WX71.go
+++ TF-CC65-2102-WX71.go
@@ -15,23 +15,37 @@ func read_csv() dataframe.DataFrame {
 		log.Fatal(err)
 	}
 	df := dataframe.ReadCSV(csv)
-	df_by_department := df.Filter(
-		dataframe.F{"dpt_cdc", series.Eq, "department"},
-		dataframe.F{"sexo", series.Eq, "M"},
-	)
-	return df_by_department
+	return df
 }
 
-/*
 func filter_by_department(df dataframe.DataFrame, department string) dataframe.DataFrame {
 	df_by_department := df.Filter(
-		// dataframe.F{"dpt_cdc", series.Eq, "department"},
+		dataframe.F{
+			Colname:    "dpt_cdc",
+			Comparator: series.Eq,
+			Comparando: department,
+		},
 	)
-	return df_by_department
+
+	group := df_by_department.GroupBy("mes_desde_inicio_covid")
+	aggre := group.Aggregation([]dataframe.AggregationType{dataframe.Aggregation_COUNT}, []string{"id_persona"})
+
+	sorted := aggre.Arrange(
+		dataframe.Sort("mes_desde_inicio_covid"),
+	)
+	return sorted
 }
-*/
 
 func main() {
 	df := read_csv()
-	fmt.Println(df)
+	department := "LIMA"
+	df_department := filter_by_department(df, department)
+	fmt.Println(df_department)
+
+	output, err := os.Create("df_by_department.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	df_department.WriteCSV(output)
+
 }
