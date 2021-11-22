d28d202fdb6a78818db8c45f14d106322f10f52a
Marcio <marciojose19@hotmail.com>
2021-11-21 23:04:22 -0500

    feat: predictions

--- frontend/vacunate-app/src/App.vue
+++ frontend/vacunate-app/src/App.vue
@@ -2,6 +2,9 @@
   <v-app>
     <v-main>
       <router-view></router-view>
+      <footer style="background-color: black; text-align: center;color: white; font-weight: bolder;padding: 10px;position: absolute;bottom: 0; width: 100%">
+        <span>Vacúnate Contra Todo Mal 2021</span>
+      </footer>
     </v-main>
   </v-app>
 </template>
--- frontend/vacunate-app/src/views/Regions.vue
+++ frontend/vacunate-app/src/views/Regions.vue
@@ -1,12 +1,18 @@
 <template>
-  <v-container>
+  <v-container style="padding-bottom: 100px">
+    <div align="center">
+      <v-row class="subtitle">
+        <p>Predicciones por región</p>
+      </v-row>
+    </div>
     <v-expansion-panels>
       <v-expansion-panel
-        v-for="(item,i) in 5"
+        v-for="(item,i) in regions"
         :key="i"
+        style="background-image:url('https://www.esan.edu.pe/conexion/actualidad/2018/10/16/1500x844_lima_2035_2050.jpg')"
       >
         <v-expansion-panel-header>
-          Item
+          {{item}}
         </v-expansion-panel-header>
         <v-expansion-panel-content>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -17,8 +23,34 @@
 </template>
 
 <script>
+  //import axios from "axios"
 export default {
   name: 'Regions',
-  components: {  }
+  data(){
+    return{
+      regions:["PUNO","LIMA","LAMBAYEQUE","ANCASH","AREQUIPA","LORETO","HUANCAVELICA","JUNIN","PIURA","CAJAMARCA",
+      "AYACUCHO","UCAYALI","TUMBES","CALLAO","LA LIBERTAD","SAN MARTIN","ICA","APURIMAC","CUSCO","HUANUCO","AMAZONAS",
+      "MADRE DE DIOS","TACNA","MOQUEGUA","PASCO"],
+      selected:undefined,
+    }
+  },
+  methods:{
+
+  },
+  created() {
+  }
 }
 </script>
+<style>
+  .subtitle {
+    text-align: center;
+    justify-content: center;
+  }
+
+  .subtitle p {
+    font-size: 2em;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+    margin: 40px;
+  }
+</style>
